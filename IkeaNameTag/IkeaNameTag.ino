621cf2a4df823fbcc83716708a4f4d313a7edca0
biedo123 <jeroenmonst@gmail.com>
2015-04-07 11:46:07 +0200

    removed useless code, made look nice, added code
    Some old code was removed, This old code was used for another project
    involving a heath+humidity sendor and bluetooth. The code should run
    smoothly now. I also orderd the code better so its prettyer to look at
    and easier to find errors in the future. there is also some code added
    in the bluetooth section, but this will (most probably) change in the
    future.

--- IkeaNameTag/IkeaNameTag.ino
+++ IkeaNameTag/IkeaNameTag.ino
@@ -15,12 +15,10 @@ int previous = LOW;    // the previous reading from the input pin
 // will quickly become a bigger number than can be stored in an int.
 long time = 0;         // the last time the output pin was toggled
 long debounce = 200;   // the debounce time, increase if the output flickers
-//////////////SWITCH CODE END///////////////
-//////////////BLUETOOTH CODE////////////////
-#define uint8 unsigned char 
-#define uint16 unsigned int
-#define uint32 unsigned long int
 
+//////////////SWITCH CODE END///////////////
+
+//////////////BLUETOOTH CODE////////////////
 #define RxD 6 // This is the pin that the Bluetooth (BT_TX) will transmit to the Arduino (RxD)
 #define TxD 7 // This is the pin that the Bluetooth (BT_RX) will receive from the Arduino (TxD)
  
@@ -35,138 +33,103 @@ SoftwareSerial blueToothSerial(RxD,TxD);
 
 void setup()
 {
-  ///////SWITCH CODE//////////////
-  pinMode(inPin, INPUT);
-  pinMode(outPin, OUTPUT);
-  ////////SWITCH CODE END//////////
-  ///////BLUETOOTH CODE////////////
-  Serial.begin(9600); // Allow Serial communication via USB cable to computer (if required)
-   pinMode(RxD, INPUT); // Setup the Arduino to receive INPUT from the bluetooth shield on Digital Pin 6
-   pinMode(TxD, OUTPUT); // Setup the Arduino to send data (OUTPUT) to the bluetooth shield on Digital Pin 7
-   pinMode(13,OUTPUT); // Use onboard LED if required.
- setupBlueToothConnection(); //Used to initialise the Bluetooth shield
+	///////SWITCH CODE//////////////
+	pinMode(inPin, INPUT);
+	pinMode(outPin, OUTPUT);
+	////////SWITCH CODE END//////////
+	///////BLUETOOTH CODE////////////
+	Serial.begin(9600); // Allow Serial communication via USB cable to computer (if required)
+	pinMode(RxD, INPUT); // Setup the Arduino to receive INPUT from the bluetooth shield on Digital Pin 6
+	pinMode(TxD, OUTPUT); // Setup the Arduino to send data (OUTPUT) to the bluetooth shield on Digital Pin 7
+	pinMode(13,OUTPUT); // Use onboard LED if required.
+	setupBlueToothConnection(); //Used to initialise the Bluetooth shield
  
- pinMode(Datapin, OUTPUT); // Setup the RGB LED Data Pin
- pinMode(Clkpin, OUTPUT); // Setup the RGB LED Clock pin
+	pinMode(Datapin, OUTPUT); // Setup the RGB LED Data Pin
+	pinMode(Clkpin, OUTPUT); // Setup the RGB LED Clock pin
 }
 
 void loop()
 {
-  //////SWITCH CODE//////////////////////////
-  reading = digitalRead(inPin);
+	 //////SWITCH CODE//////////////////////////
+	reading = digitalRead(inPin);
 
-  // if the input just went from LOW and HIGH and we've waited long enough
-  // to ignore any noise on the circuit, toggle the output pin and remember
-  // the time
-  if (reading == HIGH && previous == LOW && millis() - time > debounce) 
-  {
-    if (state == HIGH){
-      state = LOW;
-      busy = false;
+	// if the input just went from LOW and HIGH and we've waited long enough
+	// to ignore any noise on the circuit, toggle the output pin and remember
+	// the time
+	if (reading == HIGH && previous == LOW && millis() - time > debounce) 
+	{
+		if (state == HIGH)
+		{
+			state = LOW;
+			busy = false;
+		}
+		else
+		{
+			state = HIGH;
+			busy = true;
+		}
+		time = millis();
 	}
-    else{
-      state = HIGH;
-      busy = true;
+
+	digitalWrite(outPin, state);
+	previous = reading;
+	/////////SWITCH CODE END///////////////////////////
+	/////////BLUETOOTH CODE////////////////////////////
+	digitalWrite(13,LOW); //Turn off the onboard Arduino LED
+	char recvChar;
+	while(1)
+	{
+		if(blueToothSerial.available())
+		{
+			//check if there's any data sent from the remote bluetooth shield
+			recvChar = blueToothSerial.read();
+			Serial.print(recvChar);
+			if (recvChar == 'off')
+			{
+				blueToothSerial.print("false");
+			}
+			else if (recvChar == 'on')
+			{
+				blueToothSerial.print("true");
+			}
+		}
+		if(Serial.available())
+		{
+			recvChar = Serial.read();
+			//This will send value obtained (recvChar) to the phone. The value will be displayed on the phone.
+			blueToothSerial.print(recvChar);
+		}
 	}
-    time = millis();
-  }
-
-  digitalWrite(outPin, state);
-
-  previous = reading;
-  /////////SWITCH CODE END///////////////////////////
-  /////////BLUETOOTH CODE////////////////////////////
-  digitalWrite(13,LOW); //Turn off the onboard Arduino LED
-  char recvChar;
-  while(1)
-  {
-     if(blueToothSerial.available())
-     {//check if there's any data sent from the remote bluetooth shield
-       recvChar = blueToothSerial.read();
-       Serial.print(recvChar);
-       
-       if (recvChar == 'b' || recvChar == 
-     }
-if(Serial.available()){
- recvChar = Serial.read();
- 
- //This will send value obtained (recvChar) to the phone. The value will be displayed on the phone.
- blueToothSerial.print(recvChar);
- }
- }
 }
-
 ////nodig voor setup van bluetooth shield
 void setupBlueToothConnection()
 {
- blueToothSerial.begin(38400); //Set BluetoothBee BaudRate to default baud rate 38400
- blueToothSerial.print("\r\n+STWMOD=0\r\n"); //set the bluetooth work in slave mode
- blueToothSerial.print("\r\n+STNA=NaambordjeJanus\r\n"); //set the bluetooth name as "NaambordjeJanus"
-  blueToothSerial.print("\r\n+STPIN=1337\r\n");//Set SLAVE pincode"1337"
- blueToothSerial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
- blueToothSerial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
- delay(2000); // This delay is required.
- blueToothSerial.print("\r\n+INQ=1\r\n"); //make the slave bluetooth inquirable 
- delay(2000); // This delay is required.
- blueToothSerial.flush();
+	blueToothSerial.begin(38400); //Set BluetoothBee BaudRate to default baud rate 38400
+	blueToothSerial.print("\r\n+STWMOD=0\r\n"); //set the bluetooth work in slave mode
+	blueToothSerial.print("\r\n+STNA=NaambordjeJanus\r\n"); //set the bluetooth name as "NaambordjeJanus"
+	blueToothSerial.print("\r\n+STPIN=1337\r\n");//Set SLAVE pincode"1337"
+	blueToothSerial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
+	blueToothSerial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
+	delay(2000); // This delay is required.
+	blueToothSerial.print("\r\n+INQ=1\r\n"); //make the slave bluetooth inquirable 
+	delay(2000); // This delay is required.
+	blueToothSerial.flush();
 }
 //veranderd led op arduino
-void ClkProduce(void){
- digitalWrite(Clkpin, LOW);
- delayMicroseconds(20); 
- digitalWrite(Clkpin, HIGH);
- delayMicroseconds(20); 
+void ClkProduce(void)
+{
+	digitalWrite(Clkpin, LOW);
+	delayMicroseconds(20); 
+	digitalWrite(Clkpin, HIGH);
+	delayMicroseconds(20); 
 }
  
-void Send32Zero(void){
- unsigned char i;
- for (i=0; i<32; i++){
- digitalWrite(Datapin, LOW);
- ClkProduce();
- }
+void Send32Zero(void)
+{
+	unsigned char i;
+	for (i=0; i<32; i++)
+	{
+		digitalWrite(Datapin, LOW);
+		ClkProduce();
+	}
 }
- 
-uint8 TakeAntiCode(uint8 dat){
- uint8 tmp = 0;
- if ((dat & 0x80) == 0){
- tmp |= 0x02; 
- }
- 
- if ((dat & 0x40) == 0){
- tmp |= 0x01; 
- }
- 
- return tmp;
-}
- 
-// gray data
-void DatSend(uint32 dx){
- uint8 i;
- for (i=0; i<32; i++){
- if ((dx & 0x80000000) != 0){
- digitalWrite(Datapin, HIGH);
- } else {
- digitalWrite(Datapin, LOW);
- }
- 
- dx <<= 1;
- ClkProduce();
- }
-}
- 
-// data processing
-void DataDealWithAndSend(uint8 r, uint8 g, uint8 b){
- uint32 dx = 0;
- 
- dx |= (uint32)0x03 << 30; // highest two bits 1，flag bits
- dx |= (uint32)TakeAntiCode(b) << 28;
- dx |= (uint32)TakeAntiCode(g) << 26; 
- dx |= (uint32)TakeAntiCode(r) << 24;
- 
- dx |= (uint32)b << 16;
- dx |= (uint32)g << 8;
- dx |= r;
- 
- DatSend(dx);
-}
-
