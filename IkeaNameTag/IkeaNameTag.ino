d4e5533f74cde1892871ec087e8f5fe22c1815dd
biedo123 <jeroenmonst@gmail.com>
2015-05-27 11:02:50 +0200

    changed some comments
    changed an older comment that had wrong information in it

--- IkeaNameTag/IkeaNameTag.ino
+++ IkeaNameTag/IkeaNameTag.ino
@@ -105,7 +105,7 @@ void InitializeBluetooth()
 	blueToothSerial.begin(38400); //Set BluetoothBee BaudRate to default baud rate 38400
 	blueToothSerial.print("\r\n+STWMOD=0\r\n"); //set the bluetooth work in slave mode
 	blueToothSerial.print("\r\n+STNA=Naambordje\r\n"); //set the bluetooth name as "NaambordjeJanus"
-	blueToothSerial.print("\r\n+STPIN=1111\r\n");//Set SLAVE pincode"1337"
+	blueToothSerial.print("\r\n+STPIN=1111\r\n");//Set SLAVE pincode"1111"
 	blueToothSerial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
 	blueToothSerial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
 	delay(2000); // This delay is required.
